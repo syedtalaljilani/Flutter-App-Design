41da610a53a0eedb877bcbb7ba2be667909e766f
Syed Talal Jilani <talaljilani79@gmail.com>
2023-08-26 17:11:45 +0500

    home page is added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:fitness/Task%2002/app_02.dart';
+import 'home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(fontFamily: 'Poppins'),
       debugShowCheckedModeBanner: false,
       home: const SafeArea(
-        child: App02(),
+        child: Homepage(),
       ),
     );
   }
